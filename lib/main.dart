6aef988b32f54c6c720a328295a9b65efae627c4
Mayank Tiwari <neodyinfotech@gmail.com>
2026-09-15 00:27:57 +0530

    Integrate app_links deep link listener in main.dart for instant screen routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'config/app_theme.dart';
 import 'screens/login_screen.dart';
@@ -9,7 +10,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Custom Error Boundary to prevent silent white screen crashes on device unlock/resume
   ErrorWidget.builder = (FlutterErrorDetails details) {
     return MaterialApp(
@@ -71,7 +72,7 @@ void main() {
     );
   };
 
-  // Register session expired / deleted user handler 
+  // Register session expired / deleted user handler
   ApiService.onUnauthorized = () {
     final nav = navigatorKey.currentState;
     if (nav != null) {
@@ -85,8 +86,57 @@ void main() {
   runApp(const AcadovaApp());
 }
 
-class AcadovaApp extends StatelessWidget {
+class AcadovaApp extends StatefulWidget {
   const AcadovaApp({super.key});
+
+  @override
+  State<AcadovaApp> createState() => _AcadovaAppState();
+}
+
+class _AcadovaAppState extends State<AcadovaApp> {
+  late AppLinks _appLinks;
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinks();
+  }
+
+  Future<void> _initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // 1. Process initial deep link if app was launched via URL link click
+    try {
+      final initialUri = await _appLinks.getInitialLink();
+      if (initialUri != null) {
+        _handleDeepLinkUri(initialUri);
+      }
+    } catch (_) {}
+
+    // 2. Listen for deep link streams while app is running in background or foreground
+    _appLinks.uriLinkStream.listen((Uri uri) {
+      _handleDeepLinkUri(uri);
+    });
+  }
+
+  void _handleDeepLinkUri(Uri uri) {
+    if (uri.path == '/reset-password' || uri.host == 'reset-password') {
+      final token = uri.queryParameters['token'] ?? '';
+      final email = uri.queryParameters['email'] ?? '';
+      if (token.isNotEmpty && email.isNotEmpty) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          final nav = navigatorKey.currentState;
+          if (nav != null) {
+            nav.push(
+              MaterialPageRoute(
+                builder: (_) => ResetPasswordScreen(email: email, token: token),
+              ),
+            );
+          }
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
